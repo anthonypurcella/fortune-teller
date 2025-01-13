b47724c99cd22711f53993a48a9370a8974e95da
anthonypmm <purcellaanthony@gmail.com>
2025-01-12 17:54:02 -0800

    Fortune options added

--- fortune.js
+++ fortune.js
@@ -1,1 +1,5 @@
-console.log('Hello, World!');
+const fortunePromts = {
+    feeling: ['happy', 'angry', 'sad', 'depressed', 'joyful', 'scared'],
+    action: ['seek out', 'ponder', 'talk about', 'do not stop', 'stop', 'cherish'],
+    resolve: ['whats inside of yourself', 'the way you view the world', 'what friendship you have with your friends']
+}
